d1bc72af5083a8f15b0d17ba11b803eb64bf5755
Martijn Hoekstra <martijn.hoekstra@cargoxs.com>
2017-08-03 20:02:14 +0200

    Combinators on endpoints
    I'm not sure this is a fruitful direction
    fs2-http doesn't seem built for nicities,
    and that's starting to rear its head
    
    For now power on, and get some more implementation to have
    a fuller view on what's there. Then choose between
    
    * Don't have nicities (not nice)
    * Switch to http4s (and do everything again)
    * Write my own nicities (and become a http domain expert to do so)
    
    None are very appealing

--- src/main/scala/net/Endpoints.scala
+++ src/main/scala/net/Endpoints.scala
@@ -2,12 +2,28 @@ package masterleague4s
 package net
 
 import spinoco.protocol.http.Uri
+import spinoco.protocol.http.Uri._
+import masterleague4s.net.util.QueryInstances._
+import cats.implicits._
 import shapeless.tag
 import masterleague4s.data.Serialized._
+
 object Endpoints {
+  val host = Uri.parse(s"https://api.masterleague.net/").require
+  def json = Query(List("format" -> "json"))
+  def maxEntries[A](max: Int) = tag[A][Query](Query(List("page_size" -> max.toString)))
+
+  val maxHeroes = maxEntries[IdHero](20)
+  val maxMatches = maxEntries[IdMatch](25)
+  val maxTeams = maxEntries[IdTeam](100)
+  val maxPlayers = maxEntries[IdPlayer](100)
+  val maxTournaments = maxEntries[IdTournament](100)
+
   def mljsonuri(resource: String) = Uri.parse(s"https://api.masterleague.net/$resource/?format=json").require
 
-  val heroes = tag[IdHero][Uri](mljsonuri("heroes"))
+  val heroes = tag[IdHero][Uri](host.copy(path = Path(true, true, List("heroes"))).withQuery(List(json, maxHeroes).combineAll))
+
+  val heroes2 = tag[IdHero][Uri](mljsonuri("heroes"))
   val matches = tag[IdMatch][Uri](mljsonuri("matches"))
   val maps = tag[IdBattleground][Uri](mljsonuri("maps"))
   val regions = tag[IdRegion][Uri](mljsonuri("regions"))
